cb3080da7a813eee55604125af036629516e659a
DIVAKARCHAUHAN <114133300+Divakar-Rajput@users.noreply.github.com>
2024-01-07 08:53:52 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,61 +6,63 @@ let titleName = document.getElementById("title");
 
 window.onload = () => {
     if (!navigator.getBattery) {
-        alert("hello");
+        alert("");
         return false;
     }
 }
 navigator.getBattery().then((battery) => {
-    function AllBetteryUpdate(){
+    function AllBetteryUpdate() {
         connectCharger();
-        disconnentcharger();
     }
-    setInterval(AllBetteryUpdate, 1000);
+    AllBetteryUpdate();
     battery.addEventListener("chargingchange", () => {
+        AllBetteryUpdate();
+    });
+    battery.addEventListener("levelchange", () => {
         AllBetteryUpdate();
     });
 
     function connectCharger() {
-        if(battery.charging){
+        if (battery.charging) {
             batterylavel.classList.add('active');
-            TimeLeft.style.display = "none";
-            let full = (battery.level) *100;
-            if(full == 100){
+            let full = parseFloat((battery.level) * 100);
+            if (full > 99) {
+                batterylavel.classList.remove('active');
                 lowBattery.innerText = "Battery Fully Charged";
+                lowBattery.style.color = "green";
+            }
+            let currentlevel = Math.floor((battery.level) * 100);
+            batterylavel.style.height = `${currentlevel}%`;
+            BatteryText.innerText = `${currentlevel}%`;
+            titleName.innerText = `${currentlevel}% Battery Stauts`;
+
+        }
+        else {
+            batterylavel.classList.remove('active');
+
+            if (parseInt(battery.dischargingTime)) {
+                let batteryTime = (battery.dischargingTime);
+                let hour = parseInt(batteryTime / 3600);
+                let min = parseInt(batteryTime / 60 - hour * 60);
+                TimeLeft.innerText = `${hour} Hr ${min} Min remaining`;
+            }
+            let currentlevel = Math.floor((battery.level) * 100);
+            batterylavel.style.height = `${currentlevel}%`;
+            BatteryText.innerText = `${currentlevel}%`;
+            titleName.innerText = `${currentlevel}% Battery Stauts`;
+            if (currentlevel <= 20) {
+                batterylavel.style.background = "red";
+                lowBattery.innerText = `Your Battery is Low ${currentlevel}% Please Connect To Charger`;
+            }
+            else if (currentlevel > 20 && currentlevel <= 50) {
+                batterylavel.style.background = "rgb(251, 255, 0";
+            }
+            else if (currentlevel > 50 && currentlevel <= 80) {
+                batterylavel.style.background = "rgb(128, 255, 0)";
+            }
+            else {
+                batterylavel.style.background = "rgb(0, 255, 21)";
             }
         }
-        else{
-            batterylavel.classList.remove('active'); 
-            TimeLeft.style.display = "block";
-
-           if(parseInt(battery.dischargingTime)){
-             let hour = parseInt(battery.dischargingTime / 3600);
-             let min = parseInt(battery.dischargingTime / 60 - hour * 60);
-             TimeLeft.innerText = `${hour} Hr ${min} Min remaining`;
-           }
-        }
-    };
-
-
-    function disconnentcharger(){
-        let currentlevel = (battery.level) * 100 ;
-        batterylavel.style.height = `${currentlevel}%` ;
-        BatteryText.innerText = `${currentlevel}%`;
-        titleName.innerText = `${currentlevel}% Battery Stauts`;
-        if(currentlevel < 20){
-            batterylavel.style.background = "red";
-            lowBattery.innerText = `Your Battery is Low ${currentlevel}% <br> Please Connect To Charger`;
-        }
-        else if(currentlevel >= 20 && currentlevel <= 50){
-            batterylavel.style.background = "rgb(251, 255, 0";
-        }
-        else if(currentlevel > 50 && currentlevel <= 80){
-            batterylavel.style.background = "rgb(128, 255, 0)";
-        }
-        else{
-            batterylavel.style.background = "rgb(0, 255, 21)";
-        }
-        
-        
     };
 });
